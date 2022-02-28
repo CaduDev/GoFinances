1cdf9c680d454123c9d20b3ffd3d41e9f84a3bd0
carlos_kobel <carlos.arruda@kobel.com.br>
2022-02-28 15:13:56 -0300

    create screen category

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ import AppLoading from 'expo-app-loading';
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -28,7 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
